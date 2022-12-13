76eb3352a261da7f942f8a93b73654ddf8831fc5
hegyibgabor <nekeressen@gmail.com>
2022-12-13 11:38:42 +0100

    4

--- app/src/main/java/com/example/beadando_joiad3/model/DogModel.kt
+++
@@ -1,3 +0,0 @@
-package com.example.beadando_joiad3.model
-
-class DogModel(var breedName: String, var img: Int)
--- app/src/main/java/com/example/beadando_joiad3/model/apiModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.beadando_joiad3.model
-
-data class APIModel(
-    val message: List<String>,
-    val status: String
-)
